7b0aa86d2ccb3b6f33558931a285fe356b79b397
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-06-29 22:37:35 +0800

    Display user latest repos

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ searchUser.addEventListener("keyup", (e) => {
         // Show Profile
 
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
   } else {
--- ui.js
+++ ui.js
@@ -29,6 +29,26 @@ class UI {
         <div id="repos"></div>
     `;
   }
+  showRepos(repos) {
+    let output = "";
+    repos.forEach(function (repo) {
+      output += `
+            <div class="card card-body mb-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                        <span class="badge badge-primary mb-2">Stars: ${repo.stargazers_count}</span>
+                        <span class="badge badge-secondary mb-2">Watchers: ${repo.watchers_count}</span>
+                        <span class="badge badge-success mb-2">Forks: ${repo.forks_count}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+    document.getElementById("repos").innerHTML = output;
+  }
   showAlert(message, className) {
     this.clearAlert();
     const div = document.createElement("div");
